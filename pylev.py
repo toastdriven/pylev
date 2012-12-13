b9b512b2c86385a7c24a3eb23d3e4688ab4dd72b
Daniel Lindsley <daniel@toastdriven.com>
2012-12-13 00:53:09 -0600

    Implemented the faster, memoized variant for significantly better performance.

--- pylev.py
+++ pylev.py
@@ -27,17 +27,21 @@ __version__ = (1, 0, 2)
 __license__ = 'New BSD'
 
 
-def levenschtein(string_1, string_2):
+def classic_levenschtein(string_1, string_2):
     """
     Calculates the Levenschtein distance between two strings.
 
+    This version is easier to read, but significantly slower than the version
+    below (up to several orders of magnitude). Useful for learning, less so
+    otherwise.
+
     Usage::
 
-        >>> levenschtein('kitten', 'sitting')
+        >>> classic_levenschtein('kitten', 'sitting')
         3
-        >>> levenschtein('kitten', 'kitten')
+        >>> classic_levenschtein('kitten', 'kitten')
         0
-        >>> levenschtein('', '')
+        >>> classic_levenschtein('', '')
         0
 
     """
@@ -54,7 +58,54 @@ def levenschtein(string_1, string_2):
         return len_1
     else:
         return min(
-            levenschtein(string_1[1:], string_2) + 1,
-            levenschtein(string_1, string_2[1:]) + 1,
-            levenschtein(string_1[1:], string_2[1:]) + cost,
+            classic_levenschtein(string_1[1:], string_2) + 1,
+            classic_levenschtein(string_1, string_2[1:]) + 1,
+            classic_levenschtein(string_1[1:], string_2[1:]) + cost,
         )
+
+
+def levenschtein(string_1, string_2, len_1=None, len_2=None, offset_1=0, offset_2=0, memo=None):
+    """
+    Calculates the Levenschtein distance between two strings.
+
+    Usage::
+
+        >>> levenschtein('kitten', 'sitting')
+        3
+        >>> levenschtein('kitten', 'kitten')
+        0
+        >>> levenschtein('', '')
+        0
+
+    """
+    if len_1 is None:
+        len_1 = len(string_1)
+
+    if len_2 is None:
+        len_2 = len(string_2)
+
+    if memo is None:
+        memo = {}
+
+    key = ','.join([str(offset_1), str(len_1), str(offset_2), str(len_2)])
+
+    if memo.get(key) is not None:
+        return memo[key]
+
+    if len_1 == 0:
+        return len_2
+    elif len_2 == 0:
+        return len_1
+
+    cost = 0
+
+    if string_1[offset_1] != string_2[offset_2]:
+        cost = 1
+
+    dist = min(
+        levenschtein(string_1, string_2, len_1 - 1, len_2, offset_1 + 1, offset_2, memo) + 1,
+        levenschtein(string_1, string_2, len_1, len_2 - 1, offset_1, offset_2 + 1, memo) + 1,
+        levenschtein(string_1, string_2, len_1 - 1, len_2 - 1, offset_1 + 1, offset_2 + 1, memo) + cost,
+    )
+    memo[key] = dist
+    return dist
--- tests.py
+++ tests.py
@@ -1,8 +1,26 @@
-import unittest2
+import unittest
 import pylev
 
 
-class LevenschteinTestCase(unittest2.TestCase):
+class ClassicLevenschteinTestCase(unittest.TestCase):
+    def test_classic(self):
+        self.assertEqual(pylev.classic_levenschtein('kitten', 'sitting'), 3)
+
+    def test_same(self):
+        self.assertEqual(pylev.classic_levenschtein('kitten', 'kitten'), 0)
+
+    def test_empty(self):
+        self.assertEqual(pylev.classic_levenschtein('', ''), 0)
+
+    def test_long(self):
+        self.assertEqual(pylev.classic_levenschtein('confide', 'deceit'), 6)
+
+    def test_painful(self):
+        # This is pretty slow but should work.
+        self.assertEqual(pylev.classic_levenschtein('CUNsperrICY', 'conspiracy'), 8)
+
+
+class LevenschteinTestCase(unittest.TestCase):
     def test_classic(self):
         self.assertEqual(pylev.levenschtein('kitten', 'sitting'), 3)
 
@@ -14,3 +32,7 @@ class LevenschteinTestCase(unittest2.TestCase):
 
     def test_long(self):
         self.assertEqual(pylev.levenschtein('confide', 'deceit'), 6)
+
+    def test_painful(self):
+        # This is much faster than the above.
+        self.assertEqual(pylev.levenschtein('CUNsperrICY', 'conspiracy'), 8)
