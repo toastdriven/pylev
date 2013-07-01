7edf1218bc70a2d5e073c7b860a97481e93b811d
JohnDoee <supersecretemail@example.com>
2013-07-01 20:53:48 +0200

    re-implemented algorithm with a less recursive-limit sensitive one, improved previous algorithm a little both

--- tests.py
+++ tests.py
@@ -36,3 +36,26 @@ class LevenshteinTestCase(unittest.TestCase):
     def test_painful(self):
         # This is much faster than the above.
         self.assertEqual(pylev.levenshtein('CUNsperrICY', 'conspiracy'), 8)
+
+class FasterLevenshteinTestCase(unittest.TestCase):
+    def test_classic(self):
+        self.assertEqual(pylev.faster_levenshtein('kitten', 'sitting'), 3)
+
+    def test_same(self):
+        self.assertEqual(pylev.faster_levenshtein('kitten', 'kitten'), 0)
+
+    def test_empty(self):
+        self.assertEqual(pylev.faster_levenshtein('', ''), 0)
+
+    def test_long(self):
+        self.assertEqual(pylev.faster_levenshtein('confide', 'deceit'), 6)
+
+    def test_painful(self):
+        # Even even faster than the above.
+        self.assertEqual(pylev.faster_levenshtein('CUNsperrICY', 'conspiracy'), 8)
+
+    def test_threshold(self):
+        self.assertEqual(pylev.faster_levenshtein('CUNsperrICY', 'conspiracy', 2), None)
+    
+    def test_threshold_within(self):
+        self.assertEqual(pylev.faster_levenshtein('kitten', 'sitting', 3), 3)
