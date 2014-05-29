8052d5cbc5187577eec2418bf571f3f6d19ada48
Manuel Barkhau <mbarkhau@gmail.com>
2014-05-30 00:55:06 +0200

    Add method with iterative version of Wagner-Fischer algorithm.

--- pylev.py
+++ pylev.py
@@ -146,6 +146,59 @@ def wf_levenshtein(string_1, string_2):
     return d[-1]
 
 
+def wfi_levenshtein(string_1, string_2):
+    """
+    Calculates the Levenshtein distance between two strings.
+
+    This version uses an iterave version of the Wagner-Fischer algorithm.
+
+    Usage::
+
+        >>> wfi_levenshtein('kitten', 'sitting')
+        3
+        >>> wfi_levenshtein('kitten', 'kitten')
+        0
+        >>> wfi_levenshtein('', '')
+        0
+
+    """
+    if string_1 == string_2:
+        return 0
+
+    len_1 = len(string_1)
+    len_2 = len(string_2)
+
+    if len_1 == 0:
+        return len_2
+    if len_2 == 0:
+        return len_1
+
+    if len_1 > len_2:
+        string_2, string_1 = string_1, string_2
+        len_2, len_1 = len_1, len_2
+
+    d0 = [i for i in range(len_2 + 1)]
+    d1 = [j for j in range(len_2 + 1)]
+
+    for i in range(len_1):
+        d1[0] = i + 1
+        for j in range(len_2):
+            cost = d0[j]
+            if string_1[i] != string_2[j]:
+                cost += 1
+            x_cost = d1[j] + 1
+            if x_cost < cost:
+                cost = x_cost
+            y_cost = d0[j + 1] + 1
+            if y_cost < cost:
+                cost = y_cost
+            d1[j + 1] = cost
+
+        d0, d1 = d1, d0
+
+    return d0[-1]
+
+
 # Backward-compatibilty because I misspelled.
 classic_levenschtein = classic_levenshtein
 levenschtein = levenshtein
